9350745b6f0db7cfec13f4c18dfa0746a680ef06
cadent-dgates <31041734+cadent-dgates@users.noreply.github.com>
2019-10-10 15:47:09 -0400

    Generalize db connections (#6)
    * config is more backwards compatible - but not fully
    
    * ++0.11.0
    
    * can work with aws resources without creds
    
    * added help statements
    
    * updated tests for new config format

--- bigrays/config.py
+++ bigrays/config.py
@@ -1,22 +1,58 @@
-import os
-import types
+import logging
+import urllib.parse
+
+import environ
 
 
-class Config(types.SimpleNamespace):
+_logger = logging.getLogger(__name__)
+
+
+def _tuple_converter(s):
+    if isinstance(s, tuple):
+        return s
+    return tuple(s.split(','))
+
+
+@environ.config(prefix='BIGRAYS')
+class Config:
 
     # default all supported values to None
-    S3_ACCESS_KEY_ID = None
-    S3_SECRET_ACCESS_KEY = None
-    AWS_REGION = None
-    DB_USER = None
-    DB_PWD = None
-    DB_DSN = None
+    AWS_REQUIRE_SECRETS = environ.bool_var(
+        True,
+        help=('Are AWS credentials required?'
+              ' Set to False if using AWS roles or ~/.aws/credentials.'))
+    AWS_ACCESS_KEY_ID = environ.var(None)
+    AWS_SECRET_ACCESS_KEY = environ.var(None)
+    AWS_REGION = environ.var(None)
 
-    def __init__(self):
-        kwargs = {k.split('_', 1)[1]: v
-                  for k, v in os.environ.items()
-                  if k.startswith('BIGRAYS_')}
-        super().__init__(**kwargs)
+    # we could do
+    #   @environ.config
+    #   class DB
+    # here, but from the user perspective it doesn't matter
+    # and not having a nested class makes requirement checking
+    # simpler in resources.py
+    DB_UID = environ.var(None, help='UID value for odbc_connect query parameter.')
+    DB_PWD = environ.var(None, help='PWD value for odbc_connect query parameter.')
+    DB_DSN = environ.var(None, help='DSN value for odbc_connect query parameter.')
+    DB_SERVER = environ.var(None, help='DSN value for odbc_connect query parameter.')
+    DB_FLAVOR = environ.var('mssql', help='The SQL flavor, or dialect.')
+    DB_ODBC_CONNECT_PARAMS = environ.var((
+        'DB_DSN',
+        'DB_UID',
+        'DB_PWD',
+    ), converter=_tuple_converter)
+    _connect_string = '{flavor}+pyodbc:///?odbc_connect={odbc_connect}'
+
+    @property
+    def DB_CONNECT_URL(self):
+        odbc_connect = ';'.join(
+            '%s=%s' % (k.replace('DB_', ''), getattr(self, k))
+            for k in self.DB_ODBC_CONNECT_PARAMS)
+        connect_url = self._connect_string.format(
+            flavor=self.DB_FLAVOR,
+            odbc_connect=urllib.parse.quote_plus(odbc_connect)
+        )
+        return connect_url
 
 
-BigRaysConfig = Config()
+BigRaysConfig = environ.to_config(Config)
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,26 +1,42 @@
-import os
 import unittest
 from unittest import mock
 
-from bigrays.config import Config
+import environ
+
+from bigrays.config import Config, BigRaysConfig
 
 
 class TestConfig(unittest.TestCase):
-    def test(self):
-        environ = {
-            'FOO': 'z',
-            'BAR': 'y',
-            'BIGRAYS_DB_DSN': '1',
-            'BIGRAYS_DB_USER': '2',
-            'BIGRAYS_ONE_TWO': '3',
-        }
-        with mock.patch.dict('bigrays.config.os.environ', environ, clear=True):
-            config = Config()
-        self.assertEqual(config.DB_DSN, '1')
-        self.assertEqual(config.DB_USER, '2')
-        self.assertEqual(config.ONE_TWO, '3')
-        self.assertFalse(hasattr(config, 'FOO'))
-        self.assertFalse(hasattr(config, 'BAR'))
+    def test_DB_CONNECT_URL(self):
+        with mock.patch.object(BigRaysConfig, 'DB_UID', 'foo'), \
+                mock.patch.object(BigRaysConfig, 'DB_PWD', 'bar'), \
+                mock.patch.object(BigRaysConfig, 'DB_DSN', 'baz'), \
+                mock.patch.object(BigRaysConfig, 'DB_FLAVOR', 'itsmysql'), \
+                mock.patch('bigrays.config.urllib.parse.quote_plus', lambda x: x):
+            expected = 'itsmysql+pyodbc:///?odbc_connect=DSN=baz;UID=foo;PWD=bar'
+            actual = BigRaysConfig.DB_CONNECT_URL
+            self.assertEqual(actual, expected)
+
+        with mock.patch.object(BigRaysConfig, 'DB_UID', 'foo'), \
+                mock.patch.object(BigRaysConfig, 'DB_PWD', 'bar'), \
+                mock.patch.object(BigRaysConfig, 'DB_SERVER', 'baz', create=True), \
+                mock.patch.object(BigRaysConfig, 'DB_ODBC_CONNECT_PARAMS', ['DB_UID', 'DB_PWD', 'DB_SERVER']), \
+                mock.patch.object(BigRaysConfig, 'DB_FLAVOR', 'itsmysql'), \
+                mock.patch('bigrays.config.urllib.parse.quote_plus', lambda x: x):
+            expected = 'itsmysql+pyodbc:///?odbc_connect=UID=foo;PWD=bar;SERVER=baz'
+            actual = BigRaysConfig.DB_CONNECT_URL
+            self.assertEqual(actual, expected)
+
+        with mock.patch('bigrays.config.urllib.parse.quote_plus', lambda x: x):
+            config = environ.to_config(Config, {
+                'BIGRAYS_DB_SERVER': 'foo.bar.com',
+                'BIGRAYS_DB_UID': 'me',
+                'BIGRAYS_DB_ODBC_CONNECT_PARAMS': 'DB_SERVER,DB_UID',
+                'BIGRAYS_DB_FLAVOR': 'oracle'
+            })
+            actual = config.DB_CONNECT_URL
+            expected = 'oracle+pyodbc:///?odbc_connect=SERVER=foo.bar.com;UID=me'
+            self.assertEqual(actual, expected)
 
 
 if __name__ == '__main__':
